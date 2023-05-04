bc1e6c63d8f7002630914bf11abcb1cfa15775c6
João Vitor Melo Kremer <joaokremer.dev@gmail.com>
2023-05-04 17:17:16 -0300

    update: router and css reset done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,23 @@
-import React from  'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import NavBar from '../src/components/NavBar'
+import Home from '../src/pages/home/Home'
+import About from '../src/pages/about/About'
+import Projects from '../src/pages/projects/Projects';
+import Contact from  '../src/pages/contact/Contact'
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+    <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='sobre' element={<About />}/>
+        <Route path='projetos' element={<Projects />}/>
+        <Route path='contato' element={<Contact />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
